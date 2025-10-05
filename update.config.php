2ec73e858795337515da5996bbb848839c8de654
rinkelzz <55958954+rinkelzz@users.noreply.github.com>
2025-10-05 21:14:15 +0100

    Show branch time range

---
+++ update.config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'owner' => '',
+    'repository' => '',
+    'excludes' => [
+        // 'config.php',
+    ],
+    'auth' => [
+        'username' => 'admin',
+        'password_hash' => '$2y$12$v1OUgsjnzQ7o3vrZCMSxteopMaWbIoB5KGt7HlPgQuqIuMdKHo2Y2',
+    ],
+];
