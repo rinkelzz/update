c61f5daec24682d70e655a94e7fb85085245c9bf
rinkelzz <55958954+rinkelzz@users.noreply.github.com>
2025-10-06 11:09:56 +0100

    Remember target directory in config

---
+++ update.config.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'owner' => '',
+    'repository' => '',
+    'excludes' => [
+        // 'config.php',
+    ],
+    'target_directory' => __DIR__,
+    'auth' => [
+        'username' => 'admin',
+        'password_hash' => '$2y$12$v1OUgsjnzQ7o3vrZCMSxteopMaWbIoB5KGt7HlPgQuqIuMdKHo2Y2',
+    ],
+];
